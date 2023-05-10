e0516296249f41d937c2db6e5a50c45374d277fc
Sergey Lipikhin <football3@ukr.net>
2023-05-11 01:03:07 +0300

    Improve typization of Product

--- src/services/product.ts
+++ src/services/product.ts
@@ -1,7 +1,12 @@
 import { Product } from '../models/Product';
+import { WithId } from '../types/WithId';
+import { IProductMain } from '../types/IProduct';
+import { getProductMain } from '../converters/getProductMain';
 
-export async function getAll() {
-  const products = (await Product.findAll()).slice(0, 3);
+export async function getAll(): Promise<WithId<IProductMain>[]> {
+  const products = (await Product.findAll())
+    .slice(0, 11)
+    .map(getProductMain);
 
   return products;
 }
