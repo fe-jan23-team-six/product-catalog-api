67708e5a4a2654e772a92ad36463abb274d53268
Sergey Lipikhin <football3@ukr.net>
2023-05-17 05:18:11 +0300

    Proceed working with env vars

--- src/config.ts
+++ src/config.ts
@@ -1,22 +1,24 @@
+import dotenv from 'dotenv';
 import {
   SequelizeConfigLocal,
   SequelizeConfigRemote,
 } from './types/SequelizeConfig';
 
+dotenv.config();
+
 export const developmentConfig: SequelizeConfigLocal = {
-  database: 'postgres',
-  username: 'postgres',
-  password: '133043',
+  database: process.env.DB_NAME || 'postgres',
+  username: process.env.DB_USERNAME || 'postgres',
+  password: process.env.DB_PASSWORD || 'wrong password',
   options: {
-    host: 'localhost',
+    host: process.env.DB_HOST || 'localhost',
     dialect: 'postgres',
     logging: false,
   },
 };
 
 export const productionConfig: SequelizeConfigRemote = {
-  // eslint-disable-next-line max-len
-  uri: 'postgresql://lipikhin.serhii:dm8pMHI5Ycig@ep-dry-fog-719936.eu-central-1.aws.neon.tech/neondb',
+  uri: process.env.DB_URI || 'there should be uri to db',
   options: {
     dialectOptions: {
       ssl: true,
