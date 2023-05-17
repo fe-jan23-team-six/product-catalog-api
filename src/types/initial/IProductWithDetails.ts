0c14cb9580f2c6481410f26a98dcb2a1d1438e7c
Sergey Lipikhin <football3@ukr.net>
2023-05-17 03:18:27 +0300

    Remove unnecessary files

--- src/types/initial/IProductWithDetails.ts
+++
@@ -1,4 +0,0 @@
-import { IProductInitial } from './IProductInitial';
-import { IProductDetailsInitial } from './IProductDetailsInitial';
-
-export type IProductWithDetails = IProductInitial & IProductDetailsInitial;
